7964b3504eedf3e1e30fb39d2faca66f4ad907f9
slidesub <slidesub@gmail.com>
2017-11-22 18:43:45 +0800

    modified

---
+++ mercury/src/portal/article/List.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="">
+    {{msg}}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ArticleList',
+    data () {
+        return {
+            msg: 'Hello Article List!'
+        }
+    },
+    created () {
+
+    },
+    watch: {
+
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
